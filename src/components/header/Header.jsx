08d77f05c5ab71cd97087393c73679ee13aba295
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2024-04-25 14:54:27 +0100

    typed_js

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,43 @@
+import { useEffect, useRef } from "react";
 import CTA from "./CTA";
 import "./header.css";
 import ME from "../../assets/developer2.jpg";
 import HeaderSocials from "./HeaderSocials";
+import Typed from "typed.js";
 
 const Header = () => {
+  const type = useRef();
+
+  useEffect(() => {
+    var typed = new Typed(type.current, {
+      strings: [
+        "Software Developer...",
+        "FullStack Developer...",
+        "Backend Developer...",
+        "Frontend Developer...",
+        "Mobile Developer...",
+      ],
+      startDelay: 500,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 300,
+      smartBackspace: true,
+      loop: true,
+      showCursor: true,
+      cursorChar: "!",
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <header>
       <div className="container header__container">
         <h5>Hello, I&apos;m</h5>
         <h1>Jacob Adebayo</h1>
-        <h5 className="text-light">Full-Stack Developer | Mobile Developer</h5>
+        <span ref={type} className="text-light typed__js"></span>
         <CTA />
         <HeaderSocials />
 
